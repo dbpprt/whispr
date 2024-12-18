8f4e7da65dd50eae4a346a510520cd2a9ba7becf
Dennis Bappert <bappert@outlook.com>
2024-12-18 21:07:31 +0100

    refactor hotkey

--- src-tauri/src/hotkey.rs
+++ src-tauri/src/hotkey.rs
@@ -1,48 +1,20 @@
 use cocoa::base::id;
 use objc::{class, msg_send, sel, sel_impl};
+use objc::runtime::Sel;
 use anyhow::Result;
 use std::sync::Arc;
 
 type NSUInteger = libc::c_ulong;
 
-struct EventHandler {
-    callback: Arc<dyn Fn(bool) + Send + Sync>,
-}
-
-impl EventHandler {
-    fn new(callback: Arc<dyn Fn(bool) + Send + Sync>) -> Self {
-        EventHandler { callback }
-    }
-
-    fn handle_event(&self, event: id) {
-        if !event.is_null() {
-            let key_code: u16 = unsafe { msg_send![event, keyCode] };
-            
-            // Only handle right option key
-            if key_code == RIGHT_OPTION_KEY_CODE {
-                let flags: NSUInteger = unsafe { msg_send![event, modifierFlags] };
-                let is_pressed = (flags & OPTION_KEY_FLAG) != 0  // Option key flag
-                    && (flags & RIGHT_OPTION_MASK) != 0;  // Right option key mask
-                
-                println!("HotkeyManager: Right Option key - pressed: {}", is_pressed);
-                (self.callback)(is_pressed);
-            }
-        }
-    }
-}
-
-pub struct HotkeyManager {
-    #[allow(dead_code)]
-    global_monitor: Option<*mut std::ffi::c_void>, // Keep global monitor alive as raw pointer
-    #[allow(dead_code)]
-    local_monitor: Option<*mut std::ffi::c_void>, // Keep local monitor alive as raw pointer
-    event_handler: Arc<EventHandler>,
-}
-
 const NSEVENT_MASK_FLAGS_CHANGED: NSUInteger = 1 << 12;
 const RIGHT_OPTION_KEY_CODE: u16 = 61;
 const OPTION_KEY_FLAG: NSUInteger = 1 << 19;
 const RIGHT_OPTION_MASK: NSUInteger = 0x040;
+
+pub struct HotkeyManager {
+    monitors: Vec<*mut std::ffi::c_void>,
+    callback: Arc<dyn Fn(bool) + Send + Sync>,
+}
 
 impl HotkeyManager {
     pub fn new<F>(callback: F) -> Self 
@@ -50,58 +22,46 @@ impl HotkeyManager {
         F: Fn(bool) + Send + Sync + 'static,
     {
         println!("HotkeyManager: Initializing");
-        let event_handler = Arc::new(EventHandler::new(Arc::new(callback)));
         HotkeyManager {
-            global_monitor: None,
-            local_monitor: None,
-            event_handler,
+            monitors: Vec::new(),
+            callback: Arc::new(callback),
         }
     }
-    
+
+    fn add_monitor(&mut self, monitor_selector: Sel) -> Result<()> {
+        let callback = self.callback.clone();
+        let monitor: id = unsafe {
+            let handler = block::ConcreteBlock::new(move |event: id| {
+                if !event.is_null() {
+                    let key_code: u16 = msg_send![event, keyCode];
+                    if key_code == RIGHT_OPTION_KEY_CODE {
+                        let flags: NSUInteger = msg_send![event, modifierFlags];
+                        let is_pressed = (flags & OPTION_KEY_FLAG != 0) && (flags & RIGHT_OPTION_MASK != 0);
+                        println!("HotkeyManager: Right Option key - pressed: {}", is_pressed);
+                        callback(is_pressed);
+                    }
+                }
+            })
+            .copy();
+
+            msg_send![class!(NSEvent), performSelector:monitor_selector 
+                withObject:NSEVENT_MASK_FLAGS_CHANGED 
+                withObject:handler]
+        };
+
+        if monitor.is_null() {
+            return Err(anyhow::anyhow!("Failed to create event monitor"));
+        }
+
+        self.monitors.push(monitor as *mut std::ffi::c_void);
+        println!("HotkeyManager: Event monitor created");
+        Ok(())
+    }
+
     pub fn start(&mut self) -> Result<()> {
         println!("HotkeyManager: Starting event monitors");
-        let event_handler = self.event_handler.clone();
-
-        unsafe {
-            let global_monitor: id = msg_send![
-                class!(NSEvent),
-                addGlobalMonitorForEventsMatchingMask: NSEVENT_MASK_FLAGS_CHANGED
-                handler: block::ConcreteBlock::new(move |event: id| {
-                    event_handler.handle_event(event);
-                })
-                .copy()
-            ];
-
-            if global_monitor.is_null() {
-                println!("HotkeyManager: Failed to create global event monitor");
-                return Err(anyhow::anyhow!("Failed to create global event monitor"));
-            }
-
-            println!("HotkeyManager: Global event monitor created successfully");
-            self.global_monitor = Some(global_monitor as *mut std::ffi::c_void);
-        }
-
-        let event_handler = self.event_handler.clone();
-
-        unsafe {
-            let local_monitor: id = msg_send![
-                class!(NSEvent),
-                addLocalMonitorForEventsMatchingMask: NSEVENT_MASK_FLAGS_CHANGED
-                handler: block::ConcreteBlock::new(move |event: id| {
-                    event_handler.handle_event(event);
-                })
-                .copy()
-            ];
-
-            if local_monitor.is_null() {
-                println!("HotkeyManager: Failed to create local event monitor");
-                return Err(anyhow::anyhow!("Failed to create local event monitor"));
-            }
-
-            println!("HotkeyManager: Local event monitor created successfully");
-            self.local_monitor = Some(local_monitor as *mut std::ffi::c_void);
-        }
-
+        self.add_monitor(sel!(addGlobalMonitorForEventsMatchingMask:handler:))?;
+        self.add_monitor(sel!(addLocalMonitorForEventsMatchingMask:handler:))?;
         Ok(())
     }
 }
