ff12747da0e409dbedd55302e02abf618a04865d
Dennis Bappert <bappert@outlook.com>
2024-12-19 18:05:02 +0100

    restyling...

--- src-tauri/src/window.rs
+++ src-tauri/src/window.rs
@@ -1,10 +1,12 @@
-use tauri::{Window, Manager};
-use tauri_plugin_positioner::{WindowExt, Position as PositionerPosition};
+use tauri::{WebviewUrl, WebviewWindow, WebviewWindowBuilder};
+use tauri::utils::WindowEffect;
+use tauri::utils::config::WindowEffectsConfig;
+use tauri_plugin_positioner::{Position as PositionerPosition, WindowExt};
 
 const WINDOW_TITLE: &str = "whispr:overlay";
 
 pub struct OverlayWindow {
-    window: Option<Window>,
+    window: Option<WebviewWindow>,
 }
 
 impl Default for OverlayWindow {
@@ -19,55 +21,60 @@ impl OverlayWindow {
     }
 
     pub fn create_window(&mut self, app_handle: &tauri::AppHandle) {
-        println!("Creating window...");
+        let window = WebviewWindowBuilder::new(
+            app_handle,
+            WINDOW_TITLE,
+            WebviewUrl::App("index.html".into())
+        )
+        .title("whispr")
+        .inner_size(500.0, 120.0)
+        .decorations(false)
+        .transparent(true)
+        .always_on_top(true)
+        .effects(WindowEffectsConfig {
+            effects: vec![
+                // For macOS
+                WindowEffect::HudWindow,
+                // For Windows
+                WindowEffect::Acrylic,
+            ],
+            state: None,
+            radius: Some(16.0),
+            color: None,
+        })
+        .skip_taskbar(true)
+        .focused(false)
+        .visible(false)
+        .resizable(false)
+        //.theme(Some(tauri::Theme::Dark))
+        //.shadow(true)
+        //.title_bar_style(tauri::TitleBarStyle::Overlay)
+        .build()
+        .expect("Failed to create window");
 
-        // Get the predefined window
-        if let Some(window) = app_handle.get_window(WINDOW_TITLE) {
-            println!("Window found successfully");
-            
-            // Set window to be non-interactive
-            if let Err(e) = window.set_decorations(false) {
-                eprintln!("Failed to set window decorations: {}", e);
-            }
-            
-            self.window = Some(window);
-            
-            if let Some(window) = &self.window {
-                let _ = window.hide();
-                let _ = window.hide_menu();
-            }
-        } else {
-            eprintln!("Failed to get {}", WINDOW_TITLE);
+        self.window = Some(window);
+
+        if let Some(window) = &self.window {
+            let _ = window.hide();
+            let _ = window.hide_menu();
         }
     }
 
     pub fn show(&self) {
-        println!("Attempting to show window...");
         if let Some(window) = &self.window {
-            // Move window to top-right position
-            // if let Err(e) = window.move_window(PositionerPosition::TopRight) {
-            //     eprintln!("Failed to position window: {}", e);
-            // }
-
-            if let Err(e) = window.set_skip_taskbar(true) {
-                eprintln!("Failed to set skip taskbar: {}", e);
-            }
-
-            if let Err(e) = window.set_ignore_cursor_events(true) {
-                eprintln!("Failed to set ignore cursor events: {}", e);
-            }
-
-            // Show window
-            if let Err(e) = window.show() {
+            if let Err(e) = window.move_window(PositionerPosition::BottomRight)
+                .and_then(|_| window.set_skip_taskbar(true))
+                .and_then(|_| window.set_ignore_cursor_events(true))
+                .and_then(|_| window.show())
+            {
                 eprintln!("Failed to show window: {}", e);
             } else {
                 println!("Window shown successfully");
-                
+
                 if let Err(e) = window.hide_menu() {
                     eprintln!("Failed to hide window menu: {}", e);
                 }
 
-                // Ensure window is on top
                 if let Err(e) = window.set_always_on_top(true) {
                     eprintln!("Failed to set window always on top: {}", e);
                 }
@@ -78,16 +85,11 @@ impl OverlayWindow {
     }
 
     pub fn hide(&self) {
-        println!("Attempting to hide window...");
         if let Some(window) = &self.window {
-            if let Err(e) = window.hide() {
+            if let Err(e) = window.hide().and_then(|_| window.hide_menu()) {
                 eprintln!("Failed to hide window: {}", e);
             } else {
                 println!("Window hidden successfully");
-
-                if let Err(e) = window.hide_menu() {
-                    eprintln!("Failed to hide window menu: {}", e);
-                }
             }
         }
     }
