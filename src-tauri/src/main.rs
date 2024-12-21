bca15b6bd96cbc7f95ce3bb1173337f4af6c2e43
Dennis Bappert <bappert@outlook.com>
2024-12-21 10:56:29 +0100

    fix: icons were broken, turned out to be not broken.

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -265,7 +265,6 @@ fn main() {
             println!("{}, {argv:?}, {cwd}", app.package_info().name);
         }))
         .plugin(tauri_plugin_shell::init())
-        .plugin(tauri_plugin_positioner::init())
         .plugin(tauri_plugin_autostart::init(tauri_plugin_autostart::MacosLauncher::LaunchAgent, None))
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_process::init())  // Register the process plugin
